c3e4c61a47ff349129107883946b02d5de5d8433
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-15 17:14:36 -0300

    Validando os dados de entrada

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,19 +1,33 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
-  async store(req, resp) {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
     const userExists = await User.findOne({
       where: {
-        email: req.body.email,
+        email: request.body.email,
       },
     });
 
     if (userExists) {
-      return resp.status(400).json({ message: 'User already exists.' });
+      return response.status(400).json({ message: 'User already exists.' });
     }
 
-    const { id, name, email, provider } = await User.create(req.body);
-    return resp.json({
+    const { id, name, email, provider } = await User.create(request.body);
+    return response.json({
       id,
       name,
       email,
@@ -22,6 +36,24 @@ class UserController {
   }
 
   async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = request.body;
 
     const user = await User.findByPk(request.userId);
