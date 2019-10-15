ea5cc8742e66fb747424eff417225b6c2f0e17c4
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-15 15:30:22 -0300

    Criptografando a senha do usuario

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, resp) {
+    const userExists = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (userExists) {
+      return resp.status(400).json({ message: 'User already exists.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+    return resp.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
