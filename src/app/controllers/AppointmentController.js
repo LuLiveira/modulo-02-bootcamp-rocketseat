453ffc2ec88cdd8fcedbf47705bf6eb0b7f37697
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-18 17:32:30 -0300

    Criando o AppointmentController

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,48 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      date: Yup.date().required(),
+      provider_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { date, provider_id } = req.body;
+
+    /**
+     * Check if provider_id is a provider
+     */
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true,
+      },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    try {
+      const appointment = await Appointment.create({
+        user_id: req.userId,
+        provider_id,
+        date,
+      });
+
+      return res.json(appointment);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+export default new AppointmentController();
