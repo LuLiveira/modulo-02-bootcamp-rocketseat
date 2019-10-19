3fda8a5e219da596a18c6e2e630231ab703c2fd7
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-19 14:43:50 -0300

    Criando notificacao para avisar o provider de um novo agendamento utilizando o MongoDB para armazenar os dados da notificacao

---
+++ src/app/models/schemas/Notification.js
@@ -0,0 +1,24 @@
+import mongooose from 'mongoose';
+
+const NotificationSchema = new mongooose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongooose.model('Notification', NotificationSchema);
