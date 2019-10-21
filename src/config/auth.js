db67ff6a26f2eaea07740fa065e826f490376ba1
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-20 22:12:19 -0300

    Trocando configuracoes mockadas por variaveis de ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'ea53f371b19f2388a3f0e286c3f1fb62',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'root',
-  database: 'gobarber',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true, // Cria os campos created_at
     underscored: true, // Criando as tabelas no padrão ex: (user_groups)
